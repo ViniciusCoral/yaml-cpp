ea12d9cdb5dd146f1f83ab192ba0092f1dcd96ba
ViniciusCoral <68452536+ViniciusCoral@users.noreply.github.com>
2023-11-08 22:15:21 +0000

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h"
+    }
+
+    includedirs {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++20"
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
+        
